17f8e1f891c861d145e5fc37ed97bf6a3940c3f7
EliJohns268 <elijohns@udel.edu>
2025-05-05 13:56:09 -0400

    Made the basic level restart func to the enemy

---
+++ team-3-final-project/death_laser.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+
+
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		call_deferred("restart_level")
+		
+func restart_level():
+	get_tree().reload_current_scene()
