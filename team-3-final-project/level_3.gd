0e6ff60f2dfdcd3cc6c85977a62d2f08bf6243b5
AtaMakesStuff <sinankit@udel.edu>
2025-05-14 14:10:42 -0400

    Bomber & Cutscene Ammo
    - Final Level now only has 1 bullet
    - Started bomber enemy

--- team-3-final-project/level_3.gd
+++ team-3-final-project/level_3.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-signal Level3
+signal FinalLevel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Level3.emit()
+	FinalLevel.emit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
