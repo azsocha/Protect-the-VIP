d51d81335caead72b27b5104ca4078a7a7b8416e
lrizzo-4g <lrizzo@udel.edu>
2025-05-20 15:34:56 -0400

    Goal Functionality Added

--- team-3-final-project/goal_areal1.gd
+++ team-3-final-project/goal_areal1.gd
@@ -7,4 +7,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
-		get_tree().change_scene_to_file("res://Level1.tscn")
+		get_tree().change_scene_to_file("res://Level2.tscn")
