2da113e18c6d0c0f10a150fcc2afba3e4b9b7691
EliJohns268 <elijohns@udel.edu>
2025-05-20 12:27:36 -0400

    Edited the platform size and made the shooting sound

--- team-3-final-project/player_2.gd
+++ team-3-final-project/player_2.gd
@@ -3,6 +3,8 @@ extends Sprite2D
 @export var ammo = 9999999
 signal outOfAmmo
 
+@onready var shoot_sound = $ShootSound
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)  
@@ -11,6 +13,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	position = get_global_mouse_position()
 	if Input.is_action_just_pressed("click"):
+		if ammo > 0:
+			shoot_sound.play()
+		
 		texture = load("res://characterImages/crosshair2.png")
 		await get_tree().create_timer(0.3).timeout
 		texture = load("res://characterImages/crosshair.png")
