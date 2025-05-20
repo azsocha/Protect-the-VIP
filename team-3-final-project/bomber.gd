09afbb26c5c043266ca69a5bd0ef0a720d0d05b2
AtaMakesStuff <sinankit@udel.edu>
2025-05-20 16:25:45 -0400

    Bomber Blows up
    - animation is simple
    - need to add sounds?

--- team-3-final-project/bomber.gd
+++ team-3-final-project/bomber.gd
@@ -1,11 +1,10 @@
-extends Area2D
+extends CharacterBody2D
 
 signal died
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$AnimatedSprite2D.animation = "default"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,6 +12,9 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
+		print("it worked")
+		$AnimatedSprite2D.animation = "blowingUp"
+		$AnimatedSprite2D.play()
 		call_deferred("restart_level")
 		
 
