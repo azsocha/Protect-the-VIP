0507d4c68831210ff6e28da51537f344f862067c
AtaMakesStuff <sinankit@udel.edu>
2025-05-20 16:55:44 -0400

    Bomber & Images
    - Bomber now dies
    - Added final background
    - Added exit signs

--- team-3-final-project/bomber.gd
+++ team-3-final-project/bomber.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 signal died
 @onready var explodeSound = $AudioStreamPlayer2D
+var haveAmmo = true
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +12,20 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and haveAmmo:
+		var tex = $Sprite2D.texture
+		if tex == null:
+			return
+		
+		var sprite_size = tex.get_size() * scale
+		var top_left = global_position - sprite_size / 2.0
+		var mouse_pos = get_global_mouse_position()
+		var sprite_rect = Rect2(top_left, sprite_size)
+		
+		if sprite_rect.has_point(mouse_pos):
+			queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
@@ -22,3 +37,6 @@ func _on_body_entered(body: Node2D) -> void:
 
 func restart_level():
 	died.emit()
+
+func outOfAmmo() -> void:
+	haveAmmo = false
---
+++ team-3-final-project/win.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$ColorRect.hide()
+	$restartGame.hide()
+	$RichTextLabel.hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_restart_game_pressed() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)  
+	get_tree().change_scene_to_file("res://title.tscn")
+
+
+func _on_area_2d_died() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$ColorRect.show()
+	$restartGame.show()
+	$RichTextLabel.show()
