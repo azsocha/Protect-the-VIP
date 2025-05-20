f2130a09c9b6d78b082176657c3ee3fcb289aff0
EliJohns268 <elijohns@udel.edu>
2025-05-20 15:11:09 -0400

    made goal

--- team-3-final-project/goal_area_three.gd
+++ team-3-final-project/goal_area_three.gd
@@ -1,10 +1,6 @@
 extends Area2D
 
 
-
-
-
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
-		get_tree().change_scene_to_file("res://Level 2.tscn")
+		get_tree().change_scene_to_file("res://Level 3.tscn")
