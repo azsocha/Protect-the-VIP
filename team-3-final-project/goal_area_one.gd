075276e81fc0cfb2a4ed503db68509c938a085e5
AtaMakesStuff <sinankit@udel.edu>
2025-05-20 20:59:15 -0400

    Win sounds and Cleanup
    - added win sounds to goals
    - removed unused scenes and scripts
    - changed some text sizes

--- team-3-final-project/goal_area_one.gd
+++ team-3-final-project/goal_area_one.gd
@@ -7,4 +7,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
+		await get_tree().create_timer(1.6).timeout
 		get_tree().change_scene_to_file("res://Level1.tscn")
--- team-3-final-project/goal_areal2.gd
+++ team-3-final-project/goal_areal2.gd
@@ -7,4 +7,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
+		await get_tree().create_timer(1.7).timeout
 		get_tree().change_scene_to_file("res://FinalLevel.tscn")
